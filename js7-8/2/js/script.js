a92a1d9d6e03f1fb0b5e2e4931ca27b7f405dcf7
Dmitro Pokintelitsya <pokintelicad@gmail.com>
2016-04-13 16:00:43 +0300

    delay to js7-8 added

--- js7-8/2/js/script.js
+++ js7-8/2/js/script.js
@@ -1,17 +1,33 @@
 $(function () {
     $(".help>div").css({opacity: 0});
+    var timer;
 
     $("#firstname").hover(function(){
-      $("#helpFirstname").animate({opacity: '1'}, "slow");
-    },function(){$("#helpFirstname").animate({opacity: 0}, 50);
+      timer = setTimeout(function(){$("#helpFirstname").animate({opacity: '1'}, "slow")}, 500);
+    },function(){
+      if(timer) {
+      clearTimeout(timer);
+      timer = null
+    }
+    $("#helpFirstname").animate({opacity: 0}, 50);
     })
+
     $("#lastname").hover(function(){
-      $("#helpLastname").animate({opacity: '1'}, "slow");
-    },function(){$("#helpLastname").animate({opacity: 0}, 50);
+      timer = setTimeout(function(){$("#helpLastname").animate({opacity: '1'}, "slow")}, 500);
+    },function(){
+      if(timer) {
+      clearTimeout(timer);
+      timer = null
+    }$("#helpLastname").animate({opacity: 0}, 50);
     })
+
     $("#address").hover(function(){
-      $("#helpAddress").animate({opacity: '1'}, "slow");
-    },function(){$("#helpAddress").animate({opacity: 0}, 50);
+      timer = setTimeout(function(){$("#helpAddress").animate({opacity: '1'}, "slow")}, 500);
+    },function(){
+      if(timer) {
+      clearTimeout(timer);
+      timer = null
+    }$("#helpAddress").animate({opacity: 0}, 50);
     })
 
     $(show).click(function(){
