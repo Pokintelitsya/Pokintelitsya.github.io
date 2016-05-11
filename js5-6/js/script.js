10329e9f6b9beedaa6f1198e33a3522bd24e27ab
Dmitro Pokintelitsya <pokintelicad@gmail.com>
2016-05-11 10:39:35 +0300

    js5-6 changed

--- js5-6/js/script.js
+++ js5-6/js/script.js
@@ -5,38 +5,36 @@ var tm = new function Tm(){
   this.s = 0;
   this.ms = 0;
   this.time = 0;
-
+  this.currentTime = 0;
 //Timer's counting
-  var timerCount = function(){
+  var timerCount = function(startTime){
 
-      if( tm.time == 1000000000){
-        tm.time = 0;
-      }else tm.time++;
+      tm.time = new Date() - startTime;
 
-      tm.h = (Math.floor((tm.time/250)/3600))%60;
+      tm.h = (Math.floor((tm.time/1000)/3600))%60;
       if(tm.h < 10) {
         hours.innerHTML = "0"+tm.h;
       } else hours.innerHTML = tm.h;
 
-      tm.m = (Math.floor((tm.time/250)/60))%60;
+      tm.m = (Math.floor((tm.time/1000)/60))%60;
       if(tm.m < 10) {
         min.innerHTML = "0"+tm.m;
       } else min.innerHTML = tm.m;
 
-      tm.s = (Math.floor(tm.time/250))%60;
+      tm.s = (Math.floor(tm.time/1000))%60;
       if(tm.s < 10) {
         sec.innerHTML  = "0"+tm.s;
       } else sec.innerHTML  = tm.s;
 
-      tm.ms = ((tm.time*4)%1000);
+      tm.ms = ((tm.time)%1000);
       if( tm.ms < 100){
         msec.innerHTML = "0" + tm.ms;
       }else msec.innerHTML = tm.ms;
   }
 
 //Handlers
-  this.tmStart = function(){
-      tm.timerId = setInterval(timerCount, 1);
+  this.tmStart = function(sT){
+      tm.timerId = setInterval(timerCount, 25, sT);
   }
 
   this.tmStop = function(){
@@ -60,17 +58,21 @@ var tm = new function Tm(){
 function changeStartButton(){
   this.setAttribute('style','display:none');
   pause.setAttribute('style','display:inline-block');
-  tm.tmStart();
+  var startTime = new Date();
+  tm.tmStart(startTime);
 }
 function changePauseButton(){
   this.setAttribute('style','display:none');
   cont.setAttribute('style','display:inline-block');
+  tm.currentTime = tm.time;
   tm.tmPause();
 }
 function changeContButton(){
   this.setAttribute('style','display:none');
   pause.setAttribute('style','display:inline-block');
-  tm.tmStart();
+  var startTime = new Date();
+  startTime -= tm.currentTime;
+  tm.tmStart(startTime);
 }
 function changeStopButton(){
   pause.setAttribute('style','display:none');
