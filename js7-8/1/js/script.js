221dc98ec6addd4a710bb8b04ea5f92b15b72032
Dmitro Pokintelitsya <pokintelicad@gmail.com>
2016-05-27 16:55:39 +0300

    7-8 changed

--- js7-8/1/js/script.js
+++ js7-8/1/js/script.js
@@ -1,34 +1,12 @@
 $(function(){
-  var $tabs = $('.tabs');
-
+  var $tabs = $(".tabs");
+  var $content = $(".text");
+  $content.first().show();
 
   $tabs.click(function(){
-    switch ($(this).attr('id')) {
-      case 'tab-1':{
-        $('.text-1').show();
-        $('.text-2').hide();
-        $('.text-3').hide();
-      }
-
-        break;
-      case 'tab-2':{
-        $('.text-1').hide();
-        $('.text-2').show();
-        $('.text-3').hide();
-      }
-
-        break;
-      case 'tab-3':
-        $('.text-1').hide();
-        $('.text-2').hide();
-        $('.text-3').show();
-        break;
-      default:
-
-    }
-    $('.active').removeClass("active");
+    $content.hide();
+    $(this).next().show();
+    $(".active").removeClass("active");
     $(this).addClass("active");
   })
-
-
 })
--- js7-8/2/js/script.js
+++ js7-8/2/js/script.js
@@ -1,38 +1,19 @@
 $(function () {
-    $(".help>div").css({opacity: 0});
     var timer;
 
-    $("#firstname").hover(function(){
-      timer = setTimeout(function(){$("#helpFirstname").animate({opacity: '1'}, "slow")}, 500);
+    $(".input").hover(function(){
+      var animElem = $(this).parent().next();
+      timer = setTimeout(function(){
+      animElem.animate({opacity: '1'}, "slow");}, 500);
     },function(){
       if(timer) {
       clearTimeout(timer);
       timer = null
     }
-    $("#helpFirstname").animate({opacity: 0}, 50);
-    })
-
-    $("#lastname").hover(function(){
-      timer = setTimeout(function(){$("#helpLastname").animate({opacity: '1'}, "slow")}, 500);
-    },function(){
-      if(timer) {
-      clearTimeout(timer);
-      timer = null
-    }$("#helpLastname").animate({opacity: 0}, 50);
-    })
-
-    $("#address").hover(function(){
-      timer = setTimeout(function(){$("#helpAddress").animate({opacity: '1'}, "slow")}, 500);
-    },function(){
-      if(timer) {
-      clearTimeout(timer);
-      timer = null
-    }$("#helpAddress").animate({opacity: 0}, 50);
+    $(this).parent().next().animate({opacity: 0}, 50);
     })
 
     $(show).click(function(){
-      $("#helpFirstname").animate({opacity: '1'}, "slow");
-      $("#helpLastname").animate({opacity: '1'}, "slow");
-      $("#helpAddress").animate({opacity: '1'}, "slow");
+      $(".help").animate({opacity: '1'}, "slow");
     })
 })
