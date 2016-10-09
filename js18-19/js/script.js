544ac3d72810b6ba7d186d9f0823a992f46c2b8b
Dmytro <pokintelicad@gmail.com>
2016-10-09 14:39:33 +0300

    js18-19 banner+

--- js18-19/js/script.js
+++ js18-19/js/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
   $('.single-item').slick({dots: true});
+
+  var bannerItems = $('.banner-item');
+  var bannerItemsPannel = bannerItems.find('.banner-panel');
+  var collapsed = $('.banner-item-collapsed');
+  var expanded = $('.banner-item-expanded');
+  collapsed.find('.banner-status').text('-');
+  expanded.find('.banner-status').text('+');
+
+  bannerItemsPannel.click(function(){
+    var thisParent = $(this).parent();
+    if(thisParent.hasClass("banner-item-collapsed")){
+      thisParent.removeClass("banner-item-collapsed").addClass("banner-item-expanded");
+      thisParent.find('.banner-status').text('+');
+    }else {
+      if(thisParent.hasClass("banner-item-expanded")){
+        thisParent.removeClass("banner-item-expanded").addClass("banner-item-collapsed");
+        thisParent.find('.banner-status').text('-');
+      };
+    }
+
+  });
 });
