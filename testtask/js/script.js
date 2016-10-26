ecf2e5c43df9cfc178f8a3ed1363adb1a6934f5b
Dmytro <pokintelicad@gmail.com>
2016-10-27 00:15:16 +0300

    about added

--- testtask/js/script.js
+++ testtask/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-  
+  var button = $('#menu-button');
+  var menu = $('.top-menu');
+
+  button.hover(function(){menu.show().animate({opacity: 1}, 1000); button.animate({opacity: 0}, 400)},
+  function(){if(!(menu.is(':hover'))){menu.animate({opacity: 0}, 1000).hide(); button.show().animate({opacity: 1}, 1000);};});
+
+  menu.hover(function(){menu.show().animate({opacity: 1}, 1000);},
+  function(){menu.animate({opacity: 0}, 1000).hide(); button.show().animate({opacity: 1}, 1000);});
 });
